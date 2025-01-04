5e20534cb133f942f405f766932ad53541bda88e
pyr33x <pyrexrj@gmail.com>
2025-01-04 23:20:41 +0330

    refactor(cli): remove unused resolver

--- packages/cli/src/utils/config.ts
+++ packages/cli/src/utils/config.ts
@@ -1,10 +1,7 @@
 import { configSchema, type RehooksConfig } from "~/schema/config.schema";
-import { resolveImport } from "./resolver";
 import { cosmiconfig } from "cosmiconfig";
 import { handleError } from "./error";
 import { log } from "@clack/prompts";
-import path from "path";
-import fs from "fs";
 
 const explorer = cosmiconfig("rehooks", {
   searchPlaces: ["rehooks.json"],
@@ -33,33 +30,5 @@ export async function getRawConfig(cwd: string): Promise<unknown | null> {
     const errorMessage = `Error loading configuration from ${cwd}/rehooks.json: ${error}`;
     log.error(errorMessage);
     throw handleError(errorMessage);
-  }
-}
-
-export async function resolveConfigPaths(cwd: string, config: RehooksConfig) {
-  const tsConfig = await getTsConfig(cwd);
-
-  if (!tsConfig) {
-    log.warn("TypeScript configuration not found.");
-    return config;
-  }
-
-  return {
-    ...config,
-    resolvedPaths: {
-      utils: await resolveImport(config.directory, tsConfig),
-    },
-  };
-}
-
-async function getTsConfig(cwd: string): Promise<unknown | null> {
-  const tsConfigPath = path.resolve(cwd, "tsconfig.json");
-
-  try {
-    const tsConfigContent = await fs.promises.readFile(tsConfigPath, "utf-8");
-    return JSON.parse(tsConfigContent);
-  } catch (error) {
-    log.error(`Error reading tsconfig.json: ${error}`);
-    return null;
   }
 }
