e751c375f7c940d7cb90643de9f7368dbe1fb81a
pyr33x <pyrexrj@gmail.com>
2025-01-29 10:33:10 +0330

    feat(docs): useIsmorphicLayoutEffect

--- packages/core/src/useIsmorphicLayoutEffect/index.ts
+++ packages/core/src/useIsmorphicLayoutEffect/index.ts
@@ -5,12 +5,6 @@ const description =
 
 /**
  * Conditionally invokes useLayoutEffect on the server and useEffect on the client.
- *
- * @template {() => void} T - Function type for the effect callback
- * @param {T} callback - Function to execute on mount and unmount
- * @param {any[]} deps - Dependencies array for the effect
- *
- * @returns {void}
  */
 export const useIsomorphicLayoutEffect =
   typeof window !== "undefined" ? useLayoutEffect : useEffect;
