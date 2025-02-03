2eb854fc544efb8a664a38b95dc6e475aa62594a
pyr33x <pyrexrj@gmail.com>
2025-02-03 23:37:44 +0330

    ref(api): rename envschema to serverschema

--- apps/api/src/schema/server.ts
+++ apps/api/src/schema/server.ts
@@ -1,14 +1,9 @@
 import { z } from "zod";
 import "dotenv/config";
 
-const EnvSchema = z.object({
-  PORT: z
-    .string()
-    .refine(
-      (port) => parseInt(port) > 0 && parseInt(port) < 2000,
-      "Invalid port number",
-    ),
+const ServerSchema = z.object({
+  PORT: z.string().refine((port) => parseInt(port) > 0, "Invalid port number"),
 });
 
-type Env = z.infer<typeof EnvSchema>;
-export const ENV: Env = EnvSchema.parse(process.env);
+type Env = z.infer<typeof ServerSchema>;
+export const ENV: Env = ServerSchema.parse(process.env);
