37b11f9ec9b8255fb164bc0d292dd34f292dab99
pyr33x <pyrexrj@gmail.com>
2025-02-04 00:23:57 +0330

    chore(api): move funcs to utils dir

--- apps/api/src/services/hooks.service.ts
+++ apps/api/src/services/hooks.service.ts
@@ -1,3 +1,4 @@
+import { createFilter, createLimit } from "~/utilities/creators";
 import { HttpError } from "~/classes/error";
 import type { Hook } from "~/types/hook";
 
@@ -11,26 +12,11 @@ type QueryParams = {
 
 type Response = Hook[];
 
-const createFilter =
-  <T extends keyof Hook>(key: T) =>
-  (search: string) =>
-  (hook: Hook) => {
-    const value = hook[key];
-    if (typeof value === "string") {
-      return value.toLowerCase().includes(search.toLowerCase());
-    }
-    return false;
-  };
-
-const createLimit = (limit: number) => (hooks: Hook[]) => hooks.slice(0, limit);
-
 async function getHooks({ search, limit }: QueryParams): Promise<Response> {
   const hooks: Hook[] = await file.json();
 
   const applySearch = search ? createFilter("title")(search) : () => true;
-
   const filteredHooks = hooks.filter(applySearch);
-
   const limitedHooks = limit
     ? createLimit(limit)(filteredHooks)
     : filteredHooks;
@@ -38,7 +24,6 @@ async function getHooks({ search, limit }: QueryParams): Promise<Response> {
   if (!limitedHooks.length) {
     throw new HttpError("Couldn't find the requested hook.", 404);
   }
-
   return limitedHooks;
 }
 
