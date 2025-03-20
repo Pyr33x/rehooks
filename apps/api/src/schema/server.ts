4cade57a7ab464a6ca2da4e8d1d498a12cdc6313
pyr33x <pyrexrj@gmail.com>
2025-03-20 22:53:03 +0330

    chore: remove elysia/api

--- apps/api/src/schema/server.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import "dotenv/config";
-
-const ServerSchema = z.object({
-  PORT: z.string().refine((port) => parseInt(port) > 0, "Invalid port number"),
-});
-
-type Env = z.infer<typeof ServerSchema>;
-export const ENV: Env = ServerSchema.parse(process.env);
--- apps/api/src/types/server.ts
+++
@@ -1,5 +0,0 @@
-export type Server = {
-  port: number;
-  host: string;
-  docs: string;
-};
